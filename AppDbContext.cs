a6b07b92b4782febce9cd4eb5f73dab014dd4842
Adam Major <adam_major@icloud.com>
2026-09-20 14:21:48 +1200

    feat: built password storage table in db

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
     public DbSet<VaultMetadata> VaultMetadata { get; set; }
+    public DbSet<VaultStorage> VaultStorage { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -15,4 +16,13 @@ public class VaultMetadata
     public int Id { get; set; }
     public required byte[] Salt { get; set; }
     public required byte[] PasswordVerificationHash { get; set; }
+}
+
+public class VaultStorage
+{
+    public int Id { get; set; }
+    public required string ServiceName { get; set; }
+    public required string Username { get; set; }
+    public required byte[] EncryptedPassword { get; set; }
+    public required byte[] Iv { get; set; }
 }
